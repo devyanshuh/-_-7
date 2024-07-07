093d290a48a4fd8b91a82008c9b7a4326b4ff346
𝗪𝗔𝗥_𝗠𝗔𝗖𝗛𝗜𝗡𝗘 <devyanshu1526@gmail.com>
2024-07-07 17:26:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("\n𝐗𝐁𝐨𝐭𝐬 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 ⚡\nMy Master ---> @sage_xd")
+print("\n𝐗𝐁𝐨𝐭𝐬 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 ⚡\nMy Master ---> @ll_WAR_MN8_ll")
 
 
 async def main():
